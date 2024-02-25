505d42dd548e61c6da54cb6e51a73432920d181a
Gjapl <gjaplesson@gmail.com>
2024-02-25 10:49:50 +0400

    Multiple changes in components. Added routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
-// import { faTelegram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-// faMoon,
+import { faTelegram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faBars, faMoon, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRef } from "react";
-// import { Link } from "react-router-dom";
-// import { HashLink } from "react-router-hash-link";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 export function Header() {
     const navRef: any = useRef(null);
@@ -15,18 +14,18 @@ export function Header() {
 
     return (
         <header>
-            {/* <HashLink to="/#" className="link">
+            <HashLink to="/#" className="link">
                 <h2>
                     <span>
                         <FontAwesomeIcon icon={faMoon} className="moon" />
                     </span>
                     Янина Степанян
                 </h2>
-            </HashLink> */}
+            </HashLink>
             <nav ref={navRef}>
                 <ul className="nav-menu">
-                    {/* <li><HashLink to="/#information" className="link" onClick={showNavbar}>Обо мне</HashLink></li> */}
-                    {/* <li><HashLink to="/#services" className="link" onClick={showNavbar}>Услуги и цены</HashLink></li> */}
+                    <li><HashLink to="/#information" className="link" onClick={showNavbar}>Обо мне</HashLink></li>
+                    <li><HashLink to="/#services" className="link" onClick={showNavbar}>Услуги и цены</HashLink></li>
                     <li className="close"><FontAwesomeIcon icon={faTimes} onClick={showNavbar}/></li>
                 </ul>
             </nav>
@@ -36,7 +35,7 @@ export function Header() {
             <div className="contacts">
                 <h3>Контакты</h3>
                 <ul className="contact-list">
-                    {/* <li>
+                    <li>
                         <Link to="https://api.whatsapp.com/send/?phone=79258770491" target="_blank" className="link">
                             <FontAwesomeIcon icon={faWhatsapp} className="wp icon" />
                         </Link>
@@ -45,7 +44,7 @@ export function Header() {
                         <Link to="https://t.me/+79258770491" target="_blank" className="link">
                             <FontAwesomeIcon icon={faTelegram} className="tg icon" />
                         </Link>
-                    </li> */}
+                    </li>
                 </ul>
             </div>
         </header>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,21 +1,22 @@
 // import {useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Hero } from "./Hero";
 import { Info } from "./Info";
 import { Services } from "./Services";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 export function HomePage() {
-    // const { pathname } = useLocation();
+    const { pathname } = useLocation();
 
-    // useEffect(() => {
-    //     document.documentElement.scrollTo({
-    //         top: 0,
-    //         left: 0,
-    //         behavior: "instant",
-    //     });
-    // }, [pathname]);
+    useEffect(() => {
+        document.documentElement.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "instant",
+        });
+    }, [pathname]);
 
     return (
         <>
