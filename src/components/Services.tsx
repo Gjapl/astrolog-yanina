a3f29df3ab92d388a06f0cd2d685b125b948da31
Gjapl <gjaplesson@gmail.com>
2024-02-26 18:03:11 +0400

    Fixed punctuation in service descriptions

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -8,10 +8,10 @@ export function Services() {
             </h2>
             <div className="services">
                 <Service serviceName="КАРМИЧЕСКОЕ ПРЕДНАЗНАЧЕНИЕ" servicePrice={3000} hasServiceDesc1={false} serviceDesc1="" serviceDesc2={"Голосовое или письменное сообщение; возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
-                <Service serviceName="НАТАЛЬНАЯ КАРТА" servicePrice={7000} hasServiceDesc1={true} serviceDesc1={"Выполнение работы около недели"} serviceDesc2={"Голосовое или письменное сообщение, возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
-                <Service serviceName="ЭКСПРЕСС-КОНСУЛЬТАЦИЯ" servicePrice={2000} hasServiceDesc1={true} serviceDesc1="1 вопрос" serviceDesc2="Голосовое или письменное сообщение;возможно в телефонном режиме" serviceDesc3="Необходимо знать точное время рождения" />
-                <Service serviceName="СИНАСТРИЯ (взаимодействие 2-х людей)" servicePrice={5000} hasServiceDesc1={false} serviceDesc1="" serviceDesc2={"Голосовое или письменное сообщение, возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
-                <Service serviceName="ПРОГНОЗ НА ГОД" servicePrice={5000} hasServiceDesc1={true} serviceDesc1={"Выполнение работы около недели"} serviceDesc2={"Голосовое сообщение, возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
+                <Service serviceName="НАТАЛЬНАЯ КАРТА" servicePrice={7000} hasServiceDesc1={true} serviceDesc1={"Выполнение работы около недели"} serviceDesc2={"Голосовое или письменное сообщение; возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
+                <Service serviceName="ЭКСПРЕСС-КОНСУЛЬТАЦИЯ" servicePrice={2000} hasServiceDesc1={true} serviceDesc1="1 вопрос" serviceDesc2="Голосовое или письменное сообщение; возможно в телефонном режиме" serviceDesc3="Необходимо знать точное время рождения" />
+                <Service serviceName="СИНАСТРИЯ (взаимодействие 2-х людей)" servicePrice={5000} hasServiceDesc1={false} serviceDesc1="" serviceDesc2={"Голосовое или письменное сообщение; возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
+                <Service serviceName="ПРОГНОЗ НА ГОД" servicePrice={5000} hasServiceDesc1={true} serviceDesc1={"Выполнение работы около недели"} serviceDesc2={"Голосовое сообщение; возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
             </div>
         </section>
     );
