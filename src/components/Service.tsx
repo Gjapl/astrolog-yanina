7bfd19c403cf0dbf504a9f6cb0cccad035a4f0a0
Gjapl <gjaplesson@gmail.com>
2024-02-26 17:58:49 +0400

    Added new information and new service descriptions

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,14 +1,23 @@
 interface ServiceInfo {
     serviceName: string;
     servicePrice: number;
+    serviceDesc1: string,
+    serviceDesc2: string,
+    serviceDesc3: string;
+    hasServiceDesc1: boolean;
 }
 
-export function Service({serviceName, servicePrice}: ServiceInfo) {
+export function Service({serviceName, servicePrice, serviceDesc1, serviceDesc2, serviceDesc3, hasServiceDesc1}: ServiceInfo) {
     return (
         <div className="service">
             <div className="service-info">
                 <h3 className='service-name'>{serviceName}</h3>
                 <h5 className='service-price'>{servicePrice} РУБ.</h5>
+                <ul className="service-description">
+                    {hasServiceDesc1 && <li>{serviceDesc1}</li>}
+                    <li>{serviceDesc2}</li>
+                    <li>{serviceDesc3}</li>
+                </ul>
             </div>
             <hr />
         </div>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -7,10 +7,11 @@ export function Services() {
                 Услуги
             </h2>
             <div className="services">
-                <Service serviceName="КАРМИЧЕСКОЕ ПРЕДНАЗНАЧЕНИЕ" servicePrice={2500} />
-                <Service serviceName="НАТАЛЬНАЯ КАРТА" servicePrice={4500} />
-                <Service serviceName="ЭКСПРЕСС-КОНСУЛЬТАЦИЯ" servicePrice={3500} />
-                <Service serviceName="СИНАСТРИЯ" servicePrice={3000} />
+                <Service serviceName="КАРМИЧЕСКОЕ ПРЕДНАЗНАЧЕНИЕ" servicePrice={3000} hasServiceDesc1={false} serviceDesc1="" serviceDesc2={"Голосовое или письменное сообщение; возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
+                <Service serviceName="НАТАЛЬНАЯ КАРТА" servicePrice={7000} hasServiceDesc1={true} serviceDesc1={"Выполнение работы около недели"} serviceDesc2={"Голосовое или письменное сообщение, возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
+                <Service serviceName="ЭКСПРЕСС-КОНСУЛЬТАЦИЯ" servicePrice={2000} hasServiceDesc1={true} serviceDesc1="1 вопрос" serviceDesc2="Голосовое или письменное сообщение;возможно в телефонном режиме" serviceDesc3="Необходимо знать точное время рождения" />
+                <Service serviceName="СИНАСТРИЯ (взаимодействие 2-х людей)" servicePrice={5000} hasServiceDesc1={false} serviceDesc1="" serviceDesc2={"Голосовое или письменное сообщение, возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
+                <Service serviceName="ПРОГНОЗ НА ГОД" servicePrice={5000} hasServiceDesc1={true} serviceDesc1={"Выполнение работы около недели"} serviceDesc2={"Голосовое сообщение, возможно в телефонном режиме"} serviceDesc3={"Необходимо знать точное время рождения"} />
             </div>
         </section>
     );
